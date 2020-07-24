d78753dd61f51804408df6f9f89eee4e01900080
dudumax <bilfinga@gmail.com>
2020-07-25 01:19:34 +0900

    npm package

---
+++ test/test.js
@@ -0,0 +1,13 @@
+let assert = require("assert");
+let Phrase = require("../index.js");
+
+
+describe("Phrase", function() {
+  describe("#palindrome", function() {
+
+    it("should return false for non-palindrome", function() {
+
+      assert(true);
+    });
+  });
+});
